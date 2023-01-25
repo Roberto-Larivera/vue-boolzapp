e8a7e84cebee72bdbd58374ff6850e6d56d8be95
Roberto Larivera <robylariv@gmail.com>
2023-01-25 19:15:19 +0100

    Inizializzazione + integrazione vue.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+            message : 'int ok'
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+
+}).mount('#app');
